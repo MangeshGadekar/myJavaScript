b73d3e91200b233399446a9f3aefa51c6da551af
MangeshGadekar <163622826+MangeshGadekar@users.noreply.github.com>
2024-03-17 07:52:50 +0000

    done

---
+++ basics-js/dates.js
@@ -0,0 +1,17 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 10, 23);
+// let myCreatedDate = new Date("2024-01-14");  mm/dd/yyyy
+let myCreatedDate = new Date("01-12-2023");  // dd/mm/yyyy
+console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate.getTime());
+
+let currentDate = Date.now();
+
